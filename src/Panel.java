fb0eecbd1d01131a99b5ba29e401efcc6d3fdfd8
mannanej <mannanej@rose-hulman.edu>
2021-07-28 16:24:17 -0400

    Built the view and started the panel & keylist

---
+++ src/Panel.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+	private static final long serialVersionUID = 3893425775641156235L;
+
+	public Panel(JFrame frame) {
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+	}
+
+	public void update() {
+
+	}
+}
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,6 +1,48 @@
+import javax.swing.JFrame;
+
 public class Viewer {
- 
-  public Viewer() {
-    
-  }
+
+	JFrame frame;
+	Panel panel;
+	KeyList listener;
+	boolean running;
+
+	public Viewer() {
+
+		this.frame = new JFrame();
+		this.panel = new Panel(this.frame);
+		this.frame.add(this.panel);
+		this.running = true;
+
+		this.frame.setVisible(true);
+		this.frame.setTitle("Asteroids!");
+		this.frame.setSize(1275, 675);
+		this.panel.setSize(1275, 675);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void run() {
+
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0; // 60 times per second
+		double delta = 0;
+		while (this.running) {
+			long now = System.nanoTime();
+			delta = delta + ((now - lastTime) / ns);
+			lastTime = now;
+			while (delta >= 1) { // Make sure update is only happening 60 times a second
+				// handles all of the logic restricted time
+				delta--;
+				this.panel.update();
+				this.panel.repaint();
+				this.frame.repaint();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Viewer viewer = new Viewer();
+		viewer.run();
+	}
 }
